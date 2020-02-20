451c44255c1b7d23ccda8e7aaaa54d3edc371361
guoqiang <guoqiang.y@gmail.com>
2020-02-20 16:52:10 +0800

    add alt info

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function Summary () {
     <div className="card info">
       <h2>实时更新</h2>
       <li>
-      <img src="https://www.datatist.com/images/7deba3d7.code-fwh.jpeg"/>
+      <img alt="datatist" src="https://www.datatist.com/images/7deba3d7.code-fwh.jpeg"/>
       </li>
     </div>
   )
